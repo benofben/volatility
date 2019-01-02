8d0d498d064709fcc9a5543ae4592708fd99b34a
Ben Lackey <ben.lackey@outlook.com>
2019-01-01 22:32:05 -0800

    not sure what's busted...

--- preprocess.py
+++ preprocess.py
@@ -1,19 +1,29 @@
+import csv
+
 def consolidate():
+    print('Consolidating...')
     with open('data/consolidated.csv', 'w') as output:
         for index in range(1,3):
             filename = 'raw_data/stock_prices_uscomp_since_2008-01-01_file-' + str(index) + '.csv'
-            with open(filename) as inputfile:
-                for line in inputfile:
+            with open(filename) as input:
+                for line in input:
+                    # Only print one header row
                     if index != 1 and line.startswith('SECURITY_ID,TICKER'):
                         pass
                     else:
                         output.write(line)
 
 def normalize():
-    with open('data/consolidated.csv') as inputfile:
-        reader = csv.DictReader(inputfile)
-        with open('data/normalized.csv', 'w') as outputfile:
-            writer = csv.writer(outputfile)
+    print('Normalizing...')
+    with open('data/normalized.csv', 'w') as output:
+        writer = csv.writer(output)
+
+        output_row=['TICKER', 'DATE', 'HIGH', 'LOW', 'CLOSE', 'VOLUME', 'LABEL']
+        writer.writerow(output_row)
+
+        with open('data/consolidated.csv') as input:
+            reader = csv.DictReader(input)
+
             for row in reader:
                 open=float(row['OPEN'])
                 high=float(row['HIGH'])
@@ -37,6 +47,7 @@ def normalize():
                 # Unclear how to normalize date.  Need to think about this...
 
 def withhold():
+    print('Withholding...')
     pass
 
 def run():
