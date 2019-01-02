6a8f1ad72fb786c3003eb75654b7985ef1e91408
Ben Lackey <ben.lackey@outlook.com>
2019-01-01 22:00:58 -0800

    ignore and preprocess

--- preprocess.py
+++ preprocess.py
@@ -1,10 +1,18 @@
 def consolidate():
-    pass
+    with open('data/consolidated.csv', 'w') as output:
+        for index in range(1,3):
+            filename = 'raw_data/stock_prices_uscomp_since_2008-01-01_file-' + str(index) + '.csv'
+            with open(filename) as inputfile:
+                for line in inputfile:
+                    if index != 1 and line.startswith('SECURITY_ID,TICKER'):
+                        pass
+                    else:
+                        output.write(line)
 
 def normalize():
     pass
 
 def run():
-    pass
+    consolidate()
 
 run()
